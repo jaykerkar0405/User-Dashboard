5d7d3a9c91986e3fd05459f5e627defc097f38c7
Jay Kerkar <bytewise0405@gmail.com>
2025-07-02 13:06:02 +0530

    changed login page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ const LoginForm = () => {
               </div>
 
               <h1 className="text-2xl font-bold tracking-tight">
-                Welcome back
+                Welcome
               </h1>
 
               <p className="text-muted-foreground text-balance">
